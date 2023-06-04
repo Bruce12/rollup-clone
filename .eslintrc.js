9f06ca857519323f12feeaf9867381b57b7fcaff
兰建华 <lanjianhua@everjiankang.com>
2023-06-04 09:13:10 +0800

    fix: 更新 husky

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+        "mocha": true,
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        quotes: 1,
+        eqeqeq: 2,
+        'no-prototype-builtins': 0,
+    }
+}
